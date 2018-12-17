7c4d60091d06bd2c86d2a94bb4b214f5a3943519
Edison Augusthy <edisonaugusthy117@gmail.com>
2018-12-17 11:15:08 +0530

    Resolved Conflicts in Teams listing
    removed conflicts

--- src/components/Team.js
+++ src/components/Team.js
@@ -1,4 +1,4 @@
- import React, { Component } from 'react'
+import React, { Component } from 'react'
 
 export default class Team extends Component {
   state = {
@@ -6,23 +6,16 @@ export default class Team extends Component {
     contributors: []
   }
 
-  fetchContributors = async () => {
-    let response = await fetch('https://api.github.com/repos/ElixiumNetwork/elixium_core/stats/contributors', {
   fetchRepos = async () => {
     let response = await fetch('https://api.github.com/orgs/ElixiumNetwork/repos', {
       headers: {
         'Content-Type': 'application/vnd.github.hellcat-preview+json'
-        'Content-Type': 'application/vnd.github.nightshade-preview+json'
       }
     })
-    .then(res => res.json())
-      .then(res => res.json())
+     .then(res => res.json())
     this.setState({ repos: response })
-    // console.log(response)
     this.fetchContributors();
   }
-
-
 
 
   fetchContributors = async () => {
@@ -33,7 +26,7 @@ export default class Team extends Component {
           'Content-Type': 'application/vnd.github.hellcat-preview+json'
         }
       })
-        .then(res => res.json())
+      .then(res => res.json())
       userArr.push(response);
     }
     userArr = [].concat.apply([], userArr);
@@ -51,7 +44,8 @@ export default class Team extends Component {
         login: data[i].author.login,
         id: data[i].author.id,
         avatar_url: data[i].author.avatar_url,
-        html_url: data[i].author.html_url
+        html_url: data[i].author.html_url,
+        total: data[i].total
       });
     }
     // remove duplicates
@@ -62,26 +56,19 @@ export default class Team extends Component {
         values.push(value);
       }
     }
+    trimmedArray.sort((a, b) => b.total - a.total).map(c => c.author)
     this.setState({ contributors: trimmedArray })
-
-    return response.sort((a, b) => b.total - a.total).map(c => c.author)
   }
 
   openGithub = url => () => window.open(url, '_blank')
 
   componentDidMount() {
-    this.fetchContributors().then(contributors => this.setState({ contributors }))
     this.fetchRepos()
   }
 
   render() {
     return (
       <div className="Team">
-        { this.state.contributors.map(contributor => (
-          <div key={ contributor.id }  className="member" onClick={ this.openGithub(contributor.html_url) }>
-            <img src={ contributor.avatar_url } alt={ contributor.login } />
-            <h3>{ contributor.login }</h3>
-
         {this.state.contributors.map(contributor => (
           <div key={contributor.id} className="member" onClick={this.openGithub(contributor.html_url)}>
             <img src={contributor.avatar_url} alt={contributor.login} />
@@ -92,4 +79,4 @@ export default class Team extends Component {
     )
   }
 }
-}
+
