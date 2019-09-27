db5ebfa832e5aa7ccc99f2837d9357e9419bdc81
Edison Augusthy <edisonaugusthy117@gmail.com>
2019-09-27 14:18:59 +0530

    added NoProfileTile.js

---
+++ components/NoProfileTile.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default props => (
+    <div className="member" >
+        <img src='https://media0.giphy.com/media/3oEjI6SIIHBdRxXI40/giphy.gif' alt='user' />
+    </div>
+)
