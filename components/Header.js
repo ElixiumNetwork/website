7dfab7e82d5dd417582db7f6b85653e275c69ac3
Edison Augusthy <edisonaugusthy117@gmail.com>
2019-10-01 11:47:55 +0530

    Added  Meta Tags
    The Essential Meta Tags for Social Media Sharing

--- components/Header.js
+++ components/Header.js
@@ -8,6 +8,7 @@ export default props => (
     <link rel="shortcut icon" href="../static/favicon.ico" />
     <link rel="canonical" href="https://elixiumnetwork.org" />
     <link rel="preconnect" href="https://api.github.com"></link>
+    <title>Elixium | Anonymous Decentralized Application Network</title>
     <meta name="viewport" content="width=device-width, initial-scale=1"/>
     <meta name="theme-color" content="#5650A3"/>
     <meta
@@ -15,9 +16,12 @@ export default props => (
       content="Elixium is an anonymous decentralized application network that is
       language agnostic. Distributed cloud computing has never been this easy."
     />
-
-
-
-    <title>Elixium | Anonymous Decentralized Application Network</title>
+{/* meta tags for social media - shareing */}
+<meta property="og:title" content="Elixium"/>
+<meta property="og:description" content="Elixium is an anonymous decentralized application network that is
+      language agnostic. Distributed cloud computing has never been this easy"/>
+<meta property="og:image" content="https://miro.medium.com/fit/c/256/256/1*xpyxHxcpepc87qSF8z4p8Q.png"/>
+<meta property="og:url" content="https://www.elixiumnetwork.org/"/>
+<meta name="twitter:card" content="Elixium | Anonymous Decentralized Application Network"></meta>
      </Head>
 )
