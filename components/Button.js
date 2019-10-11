045d3fa39a00c3b266db53bcb357382865be2044
Edison Augusthy <edisonaugusthy117@gmail.com>
2019-10-11 14:40:54 +0530

    Update Button.js

--- components/Button.js
+++ components/Button.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default props => (
   <button
     onClick={ props.onClick }
-    className={ props.primary ? 'btn btn-primary' : 'btn btn-secondary' }
+    className={ props.buttonclass? props.buttonclass :props.primary ? 'btn btn-primary' : 'btn btn-secondary' }
     type={ props.submit ? 'submit' : 'button' }
   >
     { props.text }
